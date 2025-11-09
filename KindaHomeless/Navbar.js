938b88f3b45d8bfc8dd0facbbdd217f2beceacb5
Grayson S <grayson@infocusdata.com>
2025-11-09 03:05:29 -0600

    Fix Android status bar color and polish components.

--- KindaHomeless/Navbar.js
+++ KindaHomeless/Navbar.js
@@ -8,7 +8,7 @@ import "./global.css";
 export default function Navbar({selectedIndex, setSelectedIndex}) {
     //const [selectedIndex, setSelectedIndex] = useState(0);
         return (
-            <View className="h-24 w-full bg-emerald-900 border-t-emerald-100 border-2 absolute pb-2 bottom-0">
+            <View className="h-24 w-full bg-emerald-900 border-t-emerald-400 border-t-2 absolute pb-2 bottom-0">
                 <ButtonGroup
                     containerStyle={{ height: '80%', backgroundColor: 'transparent' , borderWidth: 0 }}
                     buttonStyle={{backgroundColor:'transparent', transitionDelay: 20}}
