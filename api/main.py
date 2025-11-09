6aeae69dc4c711ebffcfaf48d053f0e3191abf34
aklundt <andrew.klundt@utdallas.edu>
2025-11-09 01:33:14 +0000

    basic api function

---
+++ api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "You've reached the Kind-To-Homeless API"}
+
+
+@app.get("/nearby")
+async def nearby(lat: float, lon: float, radius: float):
+    return {
+        "latitude": lat,
+        "longitude": lon,
+        "radius_miles": radius,
+
+    }
+
+
